92a9bc8f160c5f19de63ddda71b04161fcd62a0c
CruiseBott <132579619+CruiseBott@users.noreply.github.com>
2023-10-30 19:47:45 +0530

    Create add.js

---
+++ add.js
@@ -0,0 +1,8 @@
+// Define a function to add two numbers
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+// Example usage of the addNumbers function
+const result = addNumbers(5, 3);
+console.log(`The sum is: ${result}`);
